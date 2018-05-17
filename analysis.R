3855de31accf130e0d84eec7e6c27c4d75660b82
Tracy Huynh <thuynh12@uw.edu>
2018-05-16 18:18:21 -0700

    Wrangled data

--- analysis.R
+++ analysis.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(httr)
 library(jsonlite)
 
+### Additional Source For Report : https://ucr.fbi.gov/hate-crime/2015/home 
+
 
 # Note : You will need to get your own API key 
 # https://api.data.gov/signup/ 
@@ -14,25 +16,35 @@ source("api-key.R")
 
 base_uri <- "https://api.usa.gov/crime/fbi/ucr/"
 endpoint_bias <- "hc/count/national/bias_name"
-endpoint_offense <- "hc/count/national/bias_name/offenses"
 
-# Change state for variation
-state <- "WA"
-endpoint_state_bias <- paste0("hc/count/states/", state, "/bias_name")
-endpoint_state_offense <- paste0("hc/count/states/", state, "/bias_name/offenses")
+# Not useful, same data divided up
+# endpoint_offense <- "hc/count/national/bias_name/offenses"
+# endpoint_state_offense <- paste0("hc/count/states/", state, "/bias_name/offenses")
 
-# Change endpoint to different variable
-endpoint <- endpoint_bias
+
 
 query <- paste0("?page=1&per_page=10&output=json&api_key=", api_key)
 
 
-resource_uri <- paste0(base_uri, endpoint, query)
+bias_resource_uri <- paste0(base_uri, endpoint_bias, query)
 
-response <- GET(resource_uri)
-body <- content(response, "text")
-body.data <- fromJSON(body)
-body.df <- flatten(body.data$results)
+bias_response <- GET(bias_resource_uri)
+bias_body <- content(bias_response, "text")
+bias_body.data <- fromJSON(bias_body)
+bias_body.df <- flatten(bias_body.data$results)
+
+# Change state for variation
+state <- "MS"
+endpoint_state_bias <- paste0("hc/count/states/", state, "/bias_name")
+
+state_resource_uri <- paste0(base_uri, endpoint_state_bias, query)
+
+state_response <- GET(state_resource_uri)
+state_body <- content(state_response, "text")
+state_body.data <- fromJSON(state_body)
+state_body.df <- flatten(state_body.data$results)
+
+
 
 ## Questions
 ## What are the possible discrepancies between major historical event and hate crimes in the past two decades?
@@ -44,3 +56,43 @@ body.df <- flatten(body.data$results)
 
 ## Compare the frequency of anti-LGBTQ hate crimes after the Defense of Marriage Act in 1996.
 
+
+
+### Hate Crimes Overall
+
+hate_crimes <- bias_body.df %>% 
+  na.omit()
+
+year <- unique(hate_crimes$year)
+
+
+# Counting all hate crimes by year
+year_combination <- data.frame(year, hate_crimes = 0)
+
+count.all <- function(df, result) {
+  for (n in year) {
+    narrow <- filter(df, year == n)
+    sum <- select(narrow, count) %>% sum()
+    result[year == n, ]$hate_crimes = sum
+  }
+  return(result)
+}
+
+year_combination <- count.all(hate_crimes, year_combination)
+
+# plot of hate crime over 25 years
+ggplot(data = year_combination) +
+  geom_bar(mapping = aes(x = year, y = hate_crimes, fill = hate_crimes), stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) # this is to make years verticle
+
+
+# State 
+state_crimes <- state_body.df %>% 
+  na.omit()
+
+
+state_combination <- data.frame(year, hate_crimes = 0)
+
+state_combination <- count.all(state_crimes, state_combination)
+
+## She broken :(
