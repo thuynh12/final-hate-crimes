a8c2965916d734fd086495dd5bbcf88826614c2c
Tracy Huynh <thuynh12@uw.edu>
2018-05-28 16:15:04 -0700

    Working example map

--- analysis.R
+++ analysis.R
@@ -82,3 +82,76 @@ test <- hate.crimes %>%
 
 # Map work
 
+#m <- geojson_read("us-map.geojson", what = "sp")
+#pal <- colorNumeric("viridis", NULL)
+
+#leaflet(data = m) %>% 
+#  addTiles() %>% 
+#  addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1,
+#              fillColor = ~pal(test$count),
+#              label = ~paste0(test$state_postal_abbr, ": ", formatC(test$count, big.mark = ","))) %>% 
+#  addLegend(pal = pal, values = ~test$count, opacity = 1)
+
+
+library(maps)
+
+data("state.fips")
+list.state <- state.fips %>% 
+  select(abb, polyname) %>% 
+  filter(!polyname %in% c("massachusetts:martha's vineyard", "massachusetts:nantucket",
+                          "michigan:north", "new york:manhattan", "new york:staten island",
+                          "new york:long island", "north carolina:knotts", "north carolina:spit",
+                          "virginia:chesapeake", "virginia:chincoteague", "washington:san juan island",
+                          "washington:lopez island", "washington:orcas island", "washington:whidbey island"))
+
+# Changing items manually
+list.state[20,]$polyname = "massachusetts"
+list.state[21,]$polyname = "michigan"
+list.state[31,]$polyname = "new york"
+list.state[32,]$polyname = "north carolina"
+list.state[45,]$polyname = "virginia"
+list.state[46,]$polyname = "washington"
+list.state[50,] = c("AL", "alaska")
+list.state[51,] = c("HI", "hawaii")
+
+# alphabetically
+list.state <- arrange(list.state, abb)
+
+list.state$polyname <- stringr::str_to_title(list.state$polyname)
+  
+
+new.map <- left_join(hate.crimes, list.state, by = c("state_postal_abbr" = "abb"))
+
+
+library(tigris)
+
+narrow.map <- new.map  %>% 
+  filter(bias_motivation == "Anti-Black or African American" & year == 2010)
+  
+states <- states(cb = T)
+
+hate.map <- narrow.map %>% 
+  group_by(polyname) %>% 
+  select(state_postal_abbr, count)
+
+merger <- geo_join(states, hate.map, "STUSPS", "state_postal_abbr")
+
+merger <- subset(merger, !is.na(count))
+pal <- colorNumeric("Reds", domain = merger$count)
+
+pop.up <- paste0("Total:", as.character(merger$count))
+
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  setView(-98.483330, 38.712046, zoom = 4) %>%
+  addPolygons(data = merger, 
+              fillColor = ~pal(merger$count),
+              fillOpacity = 0.7,
+              weight = 0.2,
+              smoothFactor = 0.2,
+              popup = ~pop.up) %>% 
+    addLegend(pal = pal, 
+              values = merger$count,
+              position = "bottomright",
+              title = "Black Hate Crimes 2010")
+  
