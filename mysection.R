fdebbf018b46eef8f1e42fb2f5e0e8c25650b9ea
Meesha Hussain <hussam2@uw.edu>
2018-05-30 15:14:30 -0700

    Meesha's part

---
+++ mysection.R
@@ -0,0 +1,77 @@
+#Loads libraries
+
+library(shiny)
+library(ggplot2)  
+library(dplyr)
+library(tidyr)
+
+# Importing raw data set
+crimes_data <- read.csv("file:///C:/Users/meesh/final-hate-crimes/hate_crime.csv", stringsAsFactors = TRUE)
+
+#Filtering data set for crimes specific to our chosen groups.
+select_groups <- filter(crimes_data, bias_motivation == c("Anti-Lesbian, Gay, Bisexual, or Transgender, Mixed Group (LGBT)", "Anti-Catholic",
+                                                          "Anti-Male Homosexual (Gay)","Anti-Female Homosexual (Lesbian)","Anti-Islamic (Muslem)",
+                                                          "Anti-Black or African American"))
+
+# Calculates the mean, meadian, min, and max for the total count in the data set. 
+bias_motivation_stats <- summarize(select_groups , mean = mean(count, na.rm = TRUE),
+                                   median = median(count, na.rm = TRUE), min = min(count, na.rm = TRUE), 
+                                   max = max(count, na.rm = TRUE))
+
+ui <- fluidPage(
+  title = "Hate Crimes by Group from 1991-2014",
+  sidebarLayout(
+    sidebarPanel(
+      conditionalPanel(
+        'input.dataset === "Bias Motivation"',
+        checkboxGroupInput("show_vars", "Columns in selected data to show:",
+                           names(select_groups), selected = names(select_groups))
+      
+    )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel("Bias Motivation", DT::dataTableOutput("mytable1")),
+      ),
+      # Plot interactive parts
+      plotOutput("plot1",
+                 click = "plot_click",
+                 dblclick = "plot_dblclick",
+                 hover = "plot_hover",
+                 brush = "plot_brush",
+                 width = "100%"
+      ),
+      verbatimTextOutput("info"),
+)
+)
+)
+
+server <- function(input, output) {
+  
+  # Choose columns to display
+  crime_data_2 = select_groups[sample(nrow(select_groups), 500), ]
+  output$mytable1 <- DT::renderDataTable({
+    DT::datatable(crime_data_2[, input$show_vars, drop = FALSE])
+
+  })
+  
+  # Plotting/Graph
+  output$plot1 <- renderPlot({
+    
+      ggplot(data = select_groups, mapping = aes(x = year, y = count, fill = bias_motivation), stat = "identity") +
+        geom_histogram(aes(color = (count %in% select_groups$bias_motivation)), size = 5) +
+        guides(color = FALSE)
+      
+      ggplot(select_groups) +
+        geom_histogram(mapping = aes(x = year, y = count, fill = bias_motivation), stat = "identity", width = .5, height = 50 ) +
+        
+      ggtitle("Number of Hate Crimes from 1991-2014 based on Minority Groups") +
+     labs(y = "Count(Number of Single Crimes Committed",
+      x = "Year (1991-2014)",
+      fill = "Motivation for Bias"
+      )
+    })
+}
+
+shinyApp(ui, server)
